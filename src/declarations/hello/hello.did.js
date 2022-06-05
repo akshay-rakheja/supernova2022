e0d5aaca346531febc526eabd8623fc5de54f6c3
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-06-05 18:20:08 -0400

    Removed old depenencies and files

--- src/declarations/hello/hello.did.js
+++
@@ -1,7 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'farewell' : IDL.Func([IDL.Text], [IDL.Text], []),
-    'greet' : IDL.Func([IDL.Text], [IDL.Text], []),
-  });
-};
-export const init = ({ IDL }) => { return []; };
--- src/hello/main.mo
+++
@@ -1,8 +0,0 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
-  public func farewell(name : Text) : async Text {
-    return "Goodbye, " # name # "!";
-  };
-};
