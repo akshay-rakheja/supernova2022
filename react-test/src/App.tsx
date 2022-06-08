9eccc65066c71897264bca6d95f5c82fb09c923b
Ray Deck <ray@raydeck.com>
2022-06-08 18:09:57 -0400

    feat: add tailwind-enabled TS-based react site

---
+++ react-test/src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { Principal } from "@dfinity/principal";
+import { AuthClient } from "@dfinity/auth-client";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
