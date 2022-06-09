6f604959397850dc815965e589f515cc72d48add
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-06-08 20:41:51 -0400

    Added identity authentication

--- react-test/src/App.tsx
+++ react-test/src/App.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Actor, HttpAgent } from "@dfinity/agent";
 import { Principal } from "@dfinity/principal";
 import { AuthClient } from "@dfinity/auth-client";
 
+// let authClient;
+
 function App() {
+  const [principal, setPrincipal] = useState<Principal | null>(null);
+  const [actor, setActor] = useState<Actor | null>(null);
+  const [authClient, setAuthClient] = useState<AuthClient | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let authClient = await AuthClient.create();
+      setAuthClient(authClient);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -22,6 +35,26 @@ function App() {
         >
           Learn React
         </a>
+        <button
+          id="signinBtn"
+          onClick={async () => {
+            console.log("YOYOYOY");
+            if (authClient) {
+              authClient.login({
+                identityProvider: "https://identity.ic0.app",
+                onSuccess: async () => {
+                  const principal = await authClient
+                    .getIdentity()
+                    .getPrincipal();
+                  setPrincipal(principal);
+                },
+              });
+            }
+          }}
+        >
+          Sign In
+        </button>
+        <p> {principal?.toString()} </p>
       </header>
     </div>
   );
