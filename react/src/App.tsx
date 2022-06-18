3e42b513a6154c5bf64d7586de1762d5c359f462
rhdeck <ray@raydeck.com>
2022-06-17 21:55:25 -0400

    fix: remove dead code in app

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -5,15 +5,11 @@ import config from "./config.json";
 import { Helmet } from "react-helmet";
 import NavigationMain from "./NavigationMain";
 import { BrowserRouter } from "react-router-dom";
-import { createBrowserHistory } from "history";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const whitelist = Object.values(
   config[config.mode as "production" | "local"].canisters
 );
-const host = config[config.mode as "production" | "local"].host;
-
-const history = createBrowserHistory({ window });
 function App() {
   return (
     <BrowserRouter>
