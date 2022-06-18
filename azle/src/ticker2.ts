f188fb7f27e1d3a739c3f56da05de3cda464bb12
rhdeck <ray@raydeck.com>
2022-06-18 17:27:25 -0400

    feat: extended ticker2 for better testing

--- azle/src/ticker2.ts
+++ azle/src/ticker2.ts
@@ -1,12 +1,36 @@
-import { Update, Query, int } from "azle";
+import { Update, Query, int, nat } from "azle";
 
-let counter: int = BigInt(1);
+//#region counter functionality
+let counter: int = BigInt(0);
 export function count(): Query<int> {
   return counter;
 }
-
 export function tick2(): Update<int> {
   console.log("Ticked me!!!!!");
-  counter = counter + BigInt(2);
+  counter = counter + BigInt(1);
   return counter;
 }
+//#region
+
+//#region counter2 functionality
+let counter2: nat = BigInt(0);
+export function count_counter2(): Query<int> {
+  return counter2;
+}
+export function counter2_tick(): Update<int> {
+  console.log("Ticked me2!!!!!");
+  counter2 = counter2 + BigInt(1);
+  return counter2;
+}
+
+//#region counter2 functionality
+let counter3: nat = BigInt(0);
+export function count_counter3(): Query<int> {
+  return counter3;
+}
+export function counter3_tick(): Update<int> {
+  console.log("Ticked me!!!!!");
+  counter3 = counter3 + BigInt(1);
+  return counter;
+}
+//#endregion
